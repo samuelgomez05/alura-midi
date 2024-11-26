f074c3717201992d36b26e5119e77ab5e7486caf
Samuel Gomez <gomezapari20@gmail.com>
2024-11-25 23:38:21 -0500

    delete files

--- desafios/desafio1/desafio1.js
+++
@@ -1,4 +0,0 @@
-// Desafio 1 - Escribe el código en JavaScript capaz de seleccionar el elemento HTML <input> de tipo teléfono:
-let tel = document.querySelector("input[type='tel']");
-
-console.log(tel);
